a4c07b4351450bfd7d86ed3e59b1bb15698ff4fb
Adsana <Adsana.Mohendran21@ycdsbk12.ca>
2021-01-20 20:11:03 -0500

    printed out csv file

--- src/CPT/DataFile.java
+++ src/CPT/DataFile.java
@@ -2,18 +2,24 @@ package CPT;
 
 import java.io.*;
 
+
 public class DataFile{
     public static void main(String[] args) throws IOException{
-    
+        // reading csv file
+
+        String line = "";
+        
         try{
-            BufferedReader br = new BufferedReader(new FileReader("PriceIndex.csv"));
-            String strPriceIndex;
-            while((strPriceIndex = br.readLine()) != null){
-                System.out.println(strPriceIndex);
+            BufferedReader br = new BufferedReader(new FileReader("C:/Users/adsan/Desktop/ICS4U1/PriceIndex.csv"));
+            while((line = br.readLine()) != null){
+                System.out.println(line);
             }
-        } catch (IOException e){
-            
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
         }
 
+      
     }
 }
