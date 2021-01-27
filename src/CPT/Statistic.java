a3539091c89cddb195c41aaf48a4d0bd6655a82d
Adsana <Adsana.Mohendran21@ycdsbk12.ca>
2021-01-26 20:22:49 -0500

    finding average

--- src/CPT/Statistic.java
+++ src/CPT/Statistic.java
@@ -1,5 +1,28 @@
 package CPT;
+import java.io.*;
+import java.util.ArrayList;
 
 public class Statistic {
-    
+
+    public static void main(String[] args) throws IOException{
+        // get list into this class
+        private Records records;
+        record = new records();
+         ArrayList<PI> list = records.getList();
+        
+        BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
+
+        // find average of the price index value for certain products
+        public void AverageOfValues(int values){
+            double dblSum;
+            int intCount;
+            double dblTotal;
+            double dblAverage;
+            for (intCount = 0; intCount < recordList.size(); intCount++){
+                dblTotal += recordList.get(intCount);
+                dblAverage = dblTotal/ recordList.size();
+                System.out.println("The average is: " + dblAverage);
+
+            }
 }
+    }
