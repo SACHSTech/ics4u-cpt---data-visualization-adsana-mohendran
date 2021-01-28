aa6225f165dab997c5703e56af096b94d2fbc9a4
Adsana <Adsana.Mohendran21@ycdsbk12.ca>
2021-01-28 09:51:17 -0500

    finding min and max

--- src/CPT/Statistic.java
+++ src/CPT/Statistic.java
@@ -31,5 +31,54 @@ public class Statistic {
                // System.out.println("The average is: " + dblAverage);
                     return dblAverage;
             }
+        
+
+            // finding max and min of values
+            public double MinAndMaxValues(){
+            ArrayList<PI> recordList = records.getRecordList();
+        
+        //declare min and max value as the first element of the list
+         double dblMin = 0;
+         double dblMax = 0;
+        dblMin = recordList.get(0);
+        dblMax = recordList.get(0);
+        
+        //declare min and max elements index as 0 (i.e. first element)
+        double dblMinIndex = 0;
+        double dblMaxIndex = 0;
+        
+        //Iterate through ArrayList
+        int intCount =0;
+        for(intCount = 1; intCount < recordList.size(); intCount++ ){
+    
+             /*
+              * If current value is less than min value, it 
+              * is new minimum value
+              */
+            
+            if( recordList.get(intCount) < dblMin ){
+                dblMin = recordList.get(intCount);
+                dblMinIndex = intCount;
+            }
+            
+            
+             /*
+              * If current value is greater than max value, it 
+              * is new maximum value.
+              */
+            
+            if( recordList.get(intCount) > dblMax ){
+                dblMax = recordList.get(intCount);
+                dblMaxIndex = intCount;
+            }
+ 
+        }
+        
+        System.out.println(" Min Value is: "  + dblMin + ", Found at index: " + dblMinIndex);
+ 
+        System.out.println("ArrayList Max Value is: " + dblMax  + ", Found at index: " + dblMaxIndex);
+        
+    }
+}
 }
     
